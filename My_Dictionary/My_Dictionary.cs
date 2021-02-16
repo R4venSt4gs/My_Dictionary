b0bb8a874b2836eb87163b23acd516803d9dca41
Raven Stags <onurquel@hotmail.com>
2021-02-16 17:11:23 +0300

    MyDictionaryDemo

---
+++ My_Dictionary/My_Dictionary.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace My_Dictionary
+{
+    class My_Dictionary<xKey, xValue>
+    {
+        xKey[] keys;
+        xValue[] values;
+
+        public My_Dictionary()
+        {
+            keys = new xKey[0];
+            values = new xValue[0];
+        }
+
+        public void Add(xKey key, xValue value)
+        {
+            xKey[] temp_Key = keys;
+            xValue[] temp_Value = values;
+
+            keys = new xKey[keys.Length + 1];
+            values = new xValue[values.Length + 1];
+
+            for (int i = 0; i < temp_Key.Length; i++)
+            {
+                keys[i] = temp_Key[i];
+                values[i] = temp_Value[i];
+            }
+
+            keys[keys.Length - 1] = key;
+            values[values.Length - 1] = value;
+        }
+
+        public void List()
+        {
+            for (int i = 0; i < keys.Length; i++)
+            {
+                Console.WriteLine(keys[i] + ":" + values[i]);
+            }
+        }
+
+        public int Count()
+        {
+            return values.Length;
+        }
+    }
+}
---
+++ My_Dictionary/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace My_Dictionary
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("<<<<< Plakalar ve Iller >>>>>");
+            Console.WriteLine(" ");
+
+            My_Dictionary<int, string> license_Plate = new My_Dictionary<int, string>();
+            license_Plate.Add(14, " Bolu");
+            license_Plate.Add(06, " Ankara");
+            license_Plate.Add(34, " Istanbul");
+            license_Plate.Add(07, " Antalya");
+            license_Plate.Add(35, " Izmir");
+
+            license_Plate.List();
+            Console.WriteLine(" ");
+            Console.WriteLine(">>>>> Liste Toplamı <<<<<");
+            Console.WriteLine(license_Plate.Count());
+        }
+    }
+}
